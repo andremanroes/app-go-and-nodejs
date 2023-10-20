8877998ff89617d98ab0caf389d20c30e36b0e48
andremanroes <andre.manurung@tiket.com>
2023-10-21 04:51:40 +0700

    fix apppp

--- frontend/app.js
+++ frontend/app.js
@@ -1,26 +1,35 @@
-const http = require('http');
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Frontend with Node.js</title>
+</head>
+<body>
+    <h1>Frontend using Node.js</h1>
+    <p id="backendResponse"></p>
+    <script>
+        const options = {
+          hostname: '34.87.81.132',
+          port: 8080,
+          path: '/',
+          method: 'GET',
+        };
 
-const options = {
-  hostname: '34.87.81.132',
-  port: 8080,
-  path: '/',
-  method: 'GET',
-};
+        const req = new XMLHttpRequest();
 
-const req = http.request(options, (res) => {
-  let data = '';
+        req.open(options.method, `http://${options.hostname}:${options.port}${options.path}`, true);
 
-  res.on('data', (chunk) => {
-    data += chunk;
-  });
+        req.onreadystatechange = function () {
+            if (req.readyState === 4 && req.status === 200) {
+                const data = req.responseText;
+                document.getElementById("backendResponse").textContent = data;
+            }
+        };
 
-  res.on('end', () => {
-    document.getElementById("backendResponse").textContent = data;
-  });
-});
+        req.onerror = function (error) {
+            console.error("Error fetching data:", error);
+        };
 
-req.on('error', (error) => {
-  console.error("Error fetching data:", error);
-});
-
-req.end();
+        req.send();
+    </script>
+</body>
+</html>
